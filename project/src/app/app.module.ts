7f0d36b9501c356e73a13f3fc4a8e7abedcf2672
VasiliadisK <kostasvasiliadis123@gmail.com>
2023-05-05 17:08:58 +0300

    unfinished header(gradient not working+logo 404)

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderCompComponent } from './header-comp/header-comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderCompComponent
   ],
   imports: [
     BrowserModule
